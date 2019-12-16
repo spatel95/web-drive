7f0605204f4ab02e115a546c5950b0f6d17c72dd
shubham <patelrsam@gmail.com>
2019-12-15 21:36:48 -0500

    making a file list

--- front-end/front-end/src/app/modules/files/drive-element/drive-element.component.ts
+++ front-end/front-end/src/app/modules/files/drive-element/drive-element.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./drive-element.component.css']
 })
 export class DriveElementComponent implements OnInit {
-  fileList = ['fileone.txt', 'file2.py', 'file3.exe']
+
+
+  fileList = [{name: 'file1.txt', size: '10 kb', creationDate: '12/12/12'},
+              {name: 'file2.txt', size: '100 kb', creationDate: '12/12/12'},
+              {name: 'file3.txt', size: '1000 kb', creationDate: '12/12/12'}];
   constructor() { }
 
   ngOnInit() {
